5bead1a804c5e791b26b297c47c215dce79143bf
unknown <@.(none)>
2013-11-02 00:25:40 +0700

    fasdfasd

---
+++ HelloStudent/Controllers/Home/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloStudent.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CallOrder(string name, string phone)
+        {
+            // todo: реализовать логику отправки email с заявкой на звонок
+
+            return new EmptyResult();
+        }
+    }
+}
